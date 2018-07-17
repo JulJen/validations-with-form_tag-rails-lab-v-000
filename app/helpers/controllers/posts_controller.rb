3f867c96c539d82ca2134aec5e63c1aaf2d1ffe4
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 16:46:05 +0000

    automatically backed up by learn

---
+++ app/helpers/controllers/posts_controller.rb
@@ -0,0 +1,44 @@
+class PostsController < ApplicationController
+
+  def index
+   @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post= Post.new(post_params)
+    if @post.valid?  #if--and only if--the author is valid
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.permit(:title, :category, :content)
+  end
+end
